084eaee58d92654df78e190d16326f742b44256b
Michael Louis Pastor <86858904+mikepastor12@users.noreply.github.com>
2024-02-14 19:21:49 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,6 +24,15 @@ def get_pdf_text(pdf_docs):
             text += page.extract_text()
     return text
 
+def get_text_chunks(text):
+    text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=800,    #  1000
+        chunk_overlap=200,
+        length_function=len
+    )
+    chunks = text_splitter.split_text(text)
+    return chunks
 
 def get_vectorstore(text_chunks):
     # embeddings = OpenAIEmbeddings()
@@ -50,6 +59,22 @@ def get_vectorstore(text_chunks):
     return vectorstore
 
 #   ###############################################
+def get_conversation_chain(vectorstore):
+    # llm = ChatOpenAI()
+    #  llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":512})
+    #  google/bigbird-roberta-base     facebook/bart-large
+    llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature": 0.5, "max_length": 512})
+
+    memory = ConversationBufferMemory(
+        memory_key='chat_history', return_messages=True)
+    conversation_chain = ConversationalRetrievalChain.from_llm(
+        llm=llm,
+        retriever=vectorstore.as_retriever(),
+        memory=memory,
+    )
+    return conversation_chain
+
+
 def handle_userinput(user_question):
 
     response = st.session_state.conversation({'question': user_question})
